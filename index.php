5a01218e13f6a57256656de15b94bb61059682ee
Renato Diniz <renatosilvadiniz@hotmail.com>
2020-10-29 16:33:01 -0300

    Criacao do header e footer a partir da index, incluindo a barra superior do menu de administrador.

--- index.php
+++ index.php
@@ -1,14 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Exame Challenge</title>
-</head>
-
-<body>
+<?= get_header(); ?>
     <h1>Olá Mundo!</h1>
-</body>
-
-</html>
+<?= get_footer(); ?>
